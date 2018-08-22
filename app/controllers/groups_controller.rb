edb17c9d4082f609424adfa7cbe16359ab27cdb7
zhaozongchang <xingyingsuifeng@gmail.com>
2018-08-22 17:08:41 +0800

    implement groups#update

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,4 +6,20 @@ class GroupsController < ApplicationController
   def show
    @group = Group.find(params[:id])
   end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    @group.update(group_params)
+    redirect_to groups_path, notice: "更新成功"
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
 end
