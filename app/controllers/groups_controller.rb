a2f2337f2bc318f817c9e98de67dfca9a46533f7
Your Name <you@example.com>
2018-08-22 17:38:04 +0800

    move form to partial

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,14 +9,14 @@ class GroupsController < ApplicationController
   end
 
   def create
-    @group = Group.new(group_params)
+     @group = Group.new(group_params)
 
-    if @group.save
-      redirect_groups_path
-    else
-      render :new
-    end
-  end
+     if @group.save
+       redirect_to groups_path
+     else
+       render :new
+     end
+   end
 
   def show
    @group = Group.find(params[:id])
